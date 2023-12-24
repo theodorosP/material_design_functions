114540bd3cf969a6eb62768bc7d864b277e56bf0
thodorisP <teosfp@hotmail.com>
2023-12-24 07:16:21 -0500

    commit

--- fix_NEB.py
+++ fix_NEB.py
@@ -3,6 +3,10 @@ import re
 
 here = str( os.getcwd() )
 
+
+#loc = the directory where the CONTCAR file exists
+#here is the current directory
+#folder is the a string 01, 02, 03 04 05 06 depending on the folder we want toa access in loc and here
 def apply_changes(loc, here, folder):
 	os.system("cp " + loc + folder + "/CONTCAR " + here + "/" + folder + "/POSCAR")
 	with open( loc + folder + "/OUTCAR", "r") as file:
